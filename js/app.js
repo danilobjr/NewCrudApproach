06828aa887ce24e80b0fa2c37e817a89e009e531
Danilo Jr <danilobjr@gmail.com>
2012-03-10 19:29:09 -0300

    primeiro commit

---
+++ js/app.js
@@ -0,0 +1,10 @@
+$(function () {
+ 
+    $('.choice').first().hover(function () {
+        $('#inovation').css('left', '7%');
+    });
+    
+    $('.choice').eq(1).hover(function () {
+        $('#inovation').css('left', '56%');
+    });
+});
